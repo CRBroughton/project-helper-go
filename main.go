9e72ec987ec5f3d9e1ef11ec07384c9e4317d1a9
CRBroughton <crbroughton@posteo.uk>
2022-08-29 12:08:35 +0100

    feat: :rocket: finish commands tutorial

--- main.go
+++ main.go
@@ -2,38 +2,73 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type Model struct {
+const url = "https://charm.sh/"
+
+type model struct {
+	status int
+	err    error
+}
+
+type statusMsg int
+
+type errMsg struct{ err error }
+
+func (e errMsg) Error() string { return e.err.Error() }
+
+func (m model) Init() tea.Cmd {
+	return checkServer
 }
 
 func main() {
-	err := tea.NewProgram(&Model{}, tea.WithAltScreen()).Start()
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func (m *Model) Init() tea.Cmd {
-	return nil
-}
-
-func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case statusMsg:
+		m.status = int(msg)
+		return m, tea.Quit
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "ctrl+q", "esc":
+		if msg.Type == tea.KeyCtrlC {
 			return m, tea.Quit
 		}
 	}
 	return m, nil
 }
 
-func (m *Model) View() string {
-	return "Hello Tea!"
+func (m model) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
+	}
+
+	s := fmt.Sprintf("Checking %s ...", url)
+
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
+	}
+	return "\n" + s + "\n\n"
+}
+
+func checkServer() tea.Msg {
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+
+	if err != nil {
+		return errMsg{err}
+	}
+
+	return statusMsg(res.StatusCode)
 }
